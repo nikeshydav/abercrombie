1d6d70ff5586f42012b09ad57d0e9fa4cbba51c1
manish@dakshsoftwares.in <manish@dakshsoftwares.in@af8a1b1a-ec74-7156-eee8-a516228f5365>
2013-12-10 10:16:54 +0000

    some changes in these files and popup also add

--- trunk/thanksyou.php
+++ trunk/thanksyou.php
@@ -22,7 +22,7 @@ Thanks,
 $headers = "From: ".$name;
 $flgSend = @mail($to, $subject, $message, $headers);
 if($flgSend){
-	echo "<div class='thank1'><h1>Thank you for showing your interest. Our advisors will revert you shortly</h1></div>";
+	echo "<div class='thank1'><h1 style='text-transform:none'> Thank you for showing interest in our Travel Services. Our destination experts will contact you at the earliest possible.<br> For any urgent travel query call 91 011 46001647 ! </h1></div>";
 }else{
 	echo "Mail Not Send";
 }
